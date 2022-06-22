5489145fc5ee0f5e2fb3305d793d6269217a2e91
LardexTheLarge <100447639+LardexTheLarge@users.noreply.github.com>
2022-06-22 14:18:45 -0600

    movie_model

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Shared
+{
+    public class Movie
+    {
+        public string MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+
+        //[NotMapped]
+        public DateTime DateReleased { get; set; }
+    }
+}
